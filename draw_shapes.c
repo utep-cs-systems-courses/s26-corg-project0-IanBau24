fcaf32894479ded5506b02e3d5c11528f4f62cab
IanBau24 <ibautistaambr@miners.utep.edu>
2026-02-12 19:35:16 -0700

    Impleteneted two functions for arrow using bitmap

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,56 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+  // 0 0 1 0 0
+  // 0 1 0 1 0
+  // 1 0 0 0 1
+  // 0 0 0 0 0
+  // 0 0 0 0 0
+  // 0 0 0 0 0
+  // 0 0 0 0 0
+  // { 0x04, 0x02, 0x01, 0x02, 0x04 } // 5e ^
+
+  void print_arrow(char a){
+    a = 0x1E; // 30 in decimal which is >
+    int b = 0xD; // 13 in decimal which is -
+
+    // doing row major lets use read in the original orientation we get our bitmap from
+    // removing 6 - row lets us  read fromt left to right instead of right to left
+    for(char row = 0; row < 7; row++){ // no 90 degree rotation
+      for(char col = 0; col < 5; col++){
+
+        unsigned short colMask = 1 << (row); // no y axis mirror image
+        // doing - first
+        unsigned short rowBitsLine = font_5x7[b][col];
+        putchar((rowBitsLine & colMask) ? '*' : ' ');
+
+        // doing > after
+        unsigned short rowBits = font_5x7[a][col];
+        putchar((rowBits & colMask) ? '*' : ' ');
+      }
+      putchar('\n');
+    }
+    putchar('\n');
+  }
+
+  void print_arrowClean(char a){
+    a = 0x1E;
+    int b = 0xD;
+
+    for(char row = 0; row < 7; row++){
+      for(char col =0; col < 5; col++){
+        unsigned short colMask = 1 << row; // do mask to compare individual bits
+        unsigned short rowBitsLine = font_5x7[13][col];  // holds -
+        unsigned short rowBits     = font_5x7[a][col];   // hold >
+
+        int dash  = rowBitsLine & colMask; // result for dash, can be 0 or 1
+        int arrow = rowBits & colMask; // result for > can be 0 or 1
+
+        putchar((dash || arrow) ? '*' : ' '); // Or operation so only one * is placed
+
+      }
+      putchar('\n');
+    }
+    putchar('\n');
+  }
+
